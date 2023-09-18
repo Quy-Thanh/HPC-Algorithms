975f423926a14420ae0a2cf896ffc58d8a7bec36
Quy-Thanh <Toquythanh2k3@gmail.com>
2023-09-18 15:16:50 +0700

    Format: bubble_sort.c

--- sequential_computing/bubble_sort.c
+++ sequential_computing/bubble_sort.c
@@ -1,34 +1,26 @@
 #include<stdio.h>			/*standard input/output*/
 
-void enter_array(int array[], int n)
-{
+void enter_array(int array[], int n){
 	int i;
-	for(i = 0; i < n; i++)
-	{
-		printf("Nhap phan tu thu %d: ", i+1);
+	for(i = 0; i < n; i++){
+		printf("array[%d] = ", i);
 		scanf("%d", &array[i]);
 	}
 }
 
-void print_array(int array[], int n)
-{
+void print_array(int array[], int n){
 	int i;
-	for(i = 0; i < n; i++)
-	{
+	for(i = 0; i < n; i++){
 		printf("%d    ", array[i]);
 	}
 	printf("\n");
 }
 
-void pubble_sort(int array[], int n)
-{
+void pubble_sort(int array[], int n){
 	int i, j;
-	for(i = 0; i < n - 1; i++)
-	{
-		for(j = n - 1; j > i; j--)
-		{
-			if(array[j] < array[j - 1])
-			{
+	for(i = 0; i < n - 1; i++){
+		for(j = n - 1; j > i; j--){
+			if(array[j] < array[j - 1]){
 				int temp = array[j];
 				array[j] = array[j - 1];
 				array[j - 1] = temp;
@@ -39,16 +31,17 @@ void pubble_sort(int array[], int n)
 
 int main(){
 	int n;
-	printf("Nhap so phan tu cua mang: ");
+	printf("Enter the number of elements in the array: ");
 	scanf("%d", &n);
 	int array[n];
 	
 	enter_array(array, n);
+	printf("The array just entered is: \n");
 	print_array(array, n);
 	
 	pubble_sort(array, n);
 	
-	printf("Mang sau khi da sap xep la: \n");
+	printf("The array after sorting is: \n");
 	print_array(array, n);
 	
 	return 0;
